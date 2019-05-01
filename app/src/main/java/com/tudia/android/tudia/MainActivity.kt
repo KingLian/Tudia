89d959fff28be999245b756090ae22007c57afd9
raadzaa <raadzaa@gmail.com>
2019-05-01 20:31:05 +0700

    Initial setup for Android Project

---
+++ app/src/main/java/com/tudia/android/tudia/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tudia.android.tudia
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
